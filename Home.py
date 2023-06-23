451a2048c4e430af40f7874dcad7ff13fafde0bf
harrychangjr <71932325+harrychangjr@users.noreply.github.com>
2023-06-23 10:59:33 +0800

    home

--- Home.py
+++ Home.py
@@ -11,5 +11,25 @@ st.sidebar.image(legalease)
 st.header("LegalEase")
 st.subheader("Home")
 st.write("*Empowering lawyers in hybrid work environments with mental health support & optimal task scheduling*")
+st.markdown("""
+Hello there!
+
+We are a group of NUS students who are aiming to solve the issue of mental health for lawyers struggling in hybrid working environments. 
+Bonded by our alma mater, Tampines Junior College (TPJC), we have built several key features which we believe can tackle the problem at hand.
+
+**Key features**: 
+- *Daily Planner (with OpenAI)* 
+- *Mood Tracker with Journal*
+- *Peer Support (Discussion Forum)*
+- *Mental Health Resources*
+
+You may also check out our demo video below, as a quick walkthrough of our application. Enjoy!
+
+~ Team TPJC, SMU-LIT Hackathon 2023
+
+""")
+
+#st.video
+st.write("*Video updated as of 23 June 2023, to be published*")
 
 st.write("*Copyright © 2023 TPJC - Harry, Brendan, Yong Jun, Ryan*") 
